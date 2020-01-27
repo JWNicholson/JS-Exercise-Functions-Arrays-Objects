1cf4b48bf47693928cf9516f85b6b75869531121
JW Nicholson <john.nicholson812@gmail.com>
2020-01-27 15:55:17 -0500

    makeSmartPerson complete

--- index.js
+++ index.js
@@ -86,9 +86,25 @@ console.log(getName(human));
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+
+  const smarty = {
+    name: name,
+    sum:function(num1, num2){
+      return num1 + num2;
+    },
+    speak:function(){
+      return 'Hello my name is ' + name;
+    },
+   
+  }
+
+  return smarty;
+  makeSmartPerson('Tom');
+  sum(8,9);
+  
 }
+
 
 
 
